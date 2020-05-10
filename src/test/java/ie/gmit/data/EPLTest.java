c64546fd763921b310781715a155f4223c516d5b
Caleb Fox <g00327455@gmit.ie>
2020-05-10 16:11:50 +0100

    testCases

--- src/test/java/ie/gmit/data/EPLTest.java
+++ src/test/java/ie/gmit/data/EPLTest.java
@@ -24,65 +24,6 @@ class EPLTest {
 
         assertEquals(code, epl.getTicketCode());
     }
-    @Test
-    public void matchTest() {
-        String code = "Test String";
 
-        assertNotEquals(code, epl.getMatch());
-
-        epl.setMatch(code);
-
-       assertEquals(code, epl.getMatch());
-    }
-    @Test
-    public void dateTest() {
-        String code = "Test String";
-
-        assertNotEquals(code, epl.getDate());
-
-        epl.setDate(code);
-
-        assertEquals(code, epl.getDate());
-    }
-    @Test
-    public void locationTest() {
-        String code = "Test String";
-
-        assertNotEquals(code, epl.getLocation());
-
-        epl.setLocation(code);
-
-        assertEquals(code, epl.getLocation());
-    }
-    @Test
-    public void seatTest() {
-        String code = "Test String";
-
-        assertNotEquals(code, epl.getSeatNumber());
-
-        epl.setSeatNumber(code);
-
-        assertEquals(code, epl.getSeatNumber());
-    }
-    @Test
-    public void ticketTest() {
-        String code = "Test String";
-
-        assertNotEquals(code, epl.getTicketNumber());
-
-        epl.setTicketNumber(code);
-
-        assertEquals(code, epl.getTicketNumber());
-    }
-    @Test
-    public void priceTest() {
-        double code = 5.0;
-
-        assertNotEquals(code, epl.getPrice());
-
-        epl.setPrice(code);
-
-        assertEquals(code, epl.getPrice());
-    }
 
 }
